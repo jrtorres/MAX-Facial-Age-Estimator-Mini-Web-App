8b52ba2d81ddd7678114fecc645fef3990a3c770
Alex Bozarth <ajbozart@us.ibm.com>
2018-10-08 15:53:50 -0700

    Revert "Add files via upload (#2)" (#4)
    This reverts commit a88d988918e1109ce7f950f23863ca6532404b3b.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, send_file, send_from_directory, after_this_request, redirect, url_for, make_response
-import flask
+from flask import Flask, render_template, request, send_file, send_from_directory
 from werkzeug import secure_filename
 import numpy as np
 from PIL import Image
@@ -10,17 +9,9 @@ import json
 from io import BytesIO
 import io
 import os
-from tempfile import NamedTemporaryFile
-from shutil import copyfileobj
-from os import remove
-import StringIO
 
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
-
-file_name = ' '
-image_name = ' '
-
 
 def image_preprocess(img_array):
     img_height, img_width, _ = np.shape(img_array)
@@ -38,26 +29,24 @@ def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
     cv2.putText(image, label, point, font, font_scale,
                 (255, 255, 255), thickness)
     
-def serve_pil_image(pil_img):
-    img_io = StringIO.StringIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
-    img_io.seek(0)
-    return send_file(img_io)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
         #Getting input image
         file_request = request.files['file']
+        global file_name 
+        global image_name 
         file_name = file_request.filename
         file_request.save(secure_filename(file_name))
-       
         
         #image pre-processing
         img_input = np.asarray(Image.open(file_name))
+        #
         image_processed, img_processed_height, img_processed_width = image_preprocess(img_input)
         #
         my_files = {'image': open(file_name, 'rb'),
@@ -88,18 +77,18 @@ def upload():
                 
                 image_processed = cv2.cvtColor(image_processed, cv2.COLOR_BGR2RGB)
                 
-                
-                image_name = 'static/' + file_name
+                global image_name 
+                image_name = 'static/img/' + file_name
                 cv2.imwrite(image_name, image_processed)
                 
+                
+                return render_template("index.html", prediction=image_name, age=pred_age)
             
-            with open(image_name, 'rb') as image_file:
-                def wsgi_app(environ, start_response):
-                        start_response('200 OK', [('Content-type', 'image/jpeg')])
-                        return image_file.read()
-                os.remove(image_name)
-                os.remove(file_name)
-                return make_response(wsgi_app)
+@app.route('/delete')
+def delete():
+    os.remove(image_name)
+    os.remove(file_name)
+    return render_template("index.html")
                 
              
 if __name__ == '__main__':
